633f81aa1dc4cda191e582a1ee3e549ce075596a
divya <divya@DESKTOP-9UVHPUM>
2021-09-03 11:24:37 +0530

    problem statement3

--- 3rd Week/src/ultramain/com/string/ProblemStatement3.java
+++ 3rd Week/src/ultramain/com/string/ProblemStatement3.java
@@ -7,14 +7,10 @@ public class ProblemStatement3
 	 */
 	public static void main(String args[])
 	{
-		String s = "C:\\IBM\\DB2\\PROGRAM\\DB2COPY1.EXE";
-		StringBuilder sb = new StringBuilder(s);
-		String[] stringarray = sb.split("\\");
-		//iterate over string array  
-		for(int i=0; i< stringarray.length; i++)  
-		{  
-		//prints the tokens  
-		System.out.println(stringarray[i]);  
+		StringBuilder sb = new StringBuilder("C:\\IBM\\DB2\\PROGRAM\\DB2COPY1.EXE");
+		System.out.println("Drive:"+ sb.substring(0, 3));
+		System.out.println("Folders:"+ sb.substring(4, 18).replace('\\','|'));
+		System.out.println("File:"+ sb.substring(19, 31));
 
 	}
 }
